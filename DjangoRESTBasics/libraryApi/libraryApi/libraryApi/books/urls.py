95cb46b769275e043684025a736851816468f71e
Anton Simeonov <aplamenov@gmail.com>
2024-11-17 14:41:21 +0200

    Django REST serializers

--- DjangoRESTBasics/libraryApi/libraryApi/libraryApi/books/urls.py
+++ DjangoRESTBasics/libraryApi/libraryApi/libraryApi/books/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from libraryApi.books import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('books/', views.list_books_view, name='list-books')
 ]
--- DjangoRESTBasics/libraryApi/libraryApi/libraryApi/books/views.py
+++ DjangoRESTBasics/libraryApi/libraryApi/libraryApi/books/views.py
@@ -1,28 +1,73 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from libraryApi.books.models import Book
+from libraryApi.books.serializers import BookSerializer
+
+# from django.http import JsonResponse
+# from django.shortcuts import render
+#
+#
+# def index(request):
+#     return JsonResponse({
+#         "name": "Anton"
+#     })
+#
+#
+# """
+# Django MPA Server-Side rendering
+#
+# books/               - Read List    - GET
+# book/<int:pk>/details- Read Details - GET
+# book/<int:pk>/create - Create       - GET/POST
+# book/<int:pk>/edit   - Update       - GET/POST
+# book/<int:pk>/delete - Delete       - GET/POST
+#
+# ---
+#
+# Django REST
+#
+# books/               - Read List    - GET
+# book/<int:pk>/       - Update, Create, Details, Delete, Partially update
+#                      - PUT,    POST  , GET    , DELETE, PATCH
+# """
 
 
-def index(request):
-    return JsonResponse({
-        "name": "Anton"
-    })
+# def list_books_view(request):
+#     books = Books.objects.all()
+#
+#     context = {
+#         'books': books,
+#     }
+#
+#     return return(request, 'some_template.html')
+#
 
+# Option without REST framework
+# def list_books_view(request):
+#     books = Book.objects.all()
+#
+#     context = {
+#         'books': books,
+#     }
+#
+#     return JsonResponse(context) # TODO parse the context to JSON
 
-"""
-Django MPA Server-Side rendering
+@api_view(['GET', 'POST',])
+def list_books_view(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
 
-books/               - Read List    - GET
-book/<int:pk>/details- Read Details - GET
-book/<int:pk>/create - Create       - GET/POST
-book/<int:pk>/edit   - Update       - GET/POST
-book/<int:pk>/delete - Delete       - GET/POST
+        serializer = BookSerializer(books, many=True)
 
----
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
 
-Django REST
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-books/               - Read List    - GET
-book/<int:pk>/       - Update, Create, Details, Delete, Partially update
-                     - PUT,    POST  , GET    , DELETE, PATCH                       
-"""
-
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
